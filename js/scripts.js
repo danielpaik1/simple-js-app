e125ebd7d9211fc1df78d543ddf21af908e074ac
danielpaik1 <danielpaik1@gmail.com>
2022-11-05 11:00:19 -0400

    added favoritefood
    added favoriteFood: kimchi

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+alert ('Hello world');
+
+
+
+/*
+let myName = 'Daniel';
+alert (myName);
+*/
+/*
+let myName = 'Daniel';
+document.write(myName);
+*/
+
+//Note: we didn't capitalize the title as this fits our style better
+let title = 'this is the title';
+
+/* This is a longer comment. It might explain, in detail, why we made a certain decision. Or, it could show examples about how to use the following code snippet. */
+
+
+let title2 = 'Hello!'; //Everything after the slashes is ignoted 
+/*
+let size = 100;
+let doubleSize = size * 2; 
+let minSize = (doubleSize * 2)- (size/2);
+document.write(minSize);
+*/
+let favoriteFood = 'Chicken';
+document.write(favoriteFood);
