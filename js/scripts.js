7c7e582497df1c57a3a62cda9708b8cb5756c9ec
danielpaik1 <danielpaik1@gmail.com>
2022-11-29 22:48:25 -0500

    creating new
    erased all and started from scratch.
    starting new project: pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-alert ('Hello world');
-
-
-
-/*
-let myName = 'Daniel';
-alert (myName);
-*/
-/*
-let myName = 'Daniel';
-document.write(myName);
-*/
-
-//Note: we didn't capitalize the title as this fits our style better
-let title = 'this is the title';
-
-/* This is a longer comment. It might explain, in detail, why we made a certain decision. Or, it could show examples about how to use the following code snippet. */
-
-
-let title2 = 'Hello!'; //Everything after the slashes is ignoted 
-/*
-let size = 100;
-let doubleSize = size * 2; 
-let minSize = (doubleSize * 2)- (size/2);
-document.write(minSize);
-*/
-let favoriteFood = 'Chicken';
-document.write(favoriteFood);
+let pokemonList= [{name: 'Bulbasaur', height: 7, types: ['grass','poison']}];
+let pokemonList= [{name: 'pikachu', height: 6, types:['electricity','speed']}];
+let pokemonList= [{name: 'charmander', height: 6, types: ['fire','speed']}];
