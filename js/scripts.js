4f322e68f6e2b378d3eee884dc4bd2f8d5abfe72
danielpaik1 <danielpaik1@gmail.com>
2022-12-01 15:29:31 -0500

    moved into one array
    moved each pokemon object into one array{1},{2},{3}

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-let pokemonList= [{name: 'Bulbasaur', height: 7, types: ['grass','poison']}];
-let pokemonList= [{name: 'pikachu', height: 6, types:['electricity','speed']}];
-let pokemonList= [{name: 'charmander', height: 6, types: ['fire','speed']}];
+let pokemonList= [{name: 'Bulbasaur'},{ height: 7}, {types: ['grass','poison']}];
+let pokemonList= [{name: 'pikachu'}, {height: 6}, {types:['electricity','speed']}];
+let pokemonList= [{name: 'charmander'}, {height: 6}, {types: ['fire','speed']}];
