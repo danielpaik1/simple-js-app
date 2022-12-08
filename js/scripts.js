66c188197862cf0d31192462591433cd81d45e89
danielpaik1 <danielpaik1@gmail.com>
2022-12-08 16:08:53 -0500

    update on pokemonList and more
    pokemonList: heights have changed from single digit to double digits with decimal points.
    type quotations have given spaces in certain areas.
    
    for ...if..else have been added for pokemonList.height > 0.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: 7,
-        type: ['grass','poison']
+        height: 0.7,
+        type: ['grass',' poison']
     },
     {
-        name: 'pikachu',
-        height: 6,
-        types:['electricity','speed']
+        name: 'Pikachu',
+        height: 0.4,
+        type:['electricity',' speed']
     },
     {
-        name: 'charmander',
-        height: 6,
-        types: ['fire','speed']
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire',' speed']
     },
 ];
+
+for (let i = 0; 
+    i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0.5) {
+      document.write(pokemonList[i].name + " (" + pokemonList[i].height + " meter) " + " is a " + pokemonList[i].type + " type! It's a large pokemon.<br>")
+    }
+    else{
+      document.write(pokemonList[i].name + " (" + pokemonList[i].height + " meter) " + " is a " + pokemonList[i].type + " type! <br>")
+    }
+  }
